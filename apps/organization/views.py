3c243d2fd3108b035262efd0132a5ee8418fbd99
acutesun <sunsec@yeah.net>
2017-07-06 15:07:03 +0800

    add org_list

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from organization.models import CityDict, CourseOrg
 
-# Create your views here.
+
+class OrgList(View):
+    def get(self, request):
+        all_city = CityDict.objects.all()
+        all_org = CourseOrg.objects.all()
+        org_nums = all_org.count()   # 课程机构数量
+        context = {
+            'all_city': all_city,
+            'all_org': all_org,
+            'org_nums': org_nums,
+        }
+        return render(request, 'org-list.html', context)
+
+
